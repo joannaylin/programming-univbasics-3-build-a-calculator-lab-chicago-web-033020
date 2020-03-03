4fded6baf7a4698b29890aa4e677214e51884961
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-03 23:33:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,22 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+    subtract = num1 - num2
+    return subtract
 end
 
 def division(num1, num2)
-
+    divide = num1/num2
+    return divide
 end
 
 def multiplication(num1, num2)
-
+    multiply = num1 * num2
+    return multiply
 end
 
 def modulo(num1, num2)
-
+    remainder = num2 % num1
 end
 
 def square_root(num)
